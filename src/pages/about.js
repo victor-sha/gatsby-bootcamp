e762e4c79fa05dbee283d29989608e38bb8550e5
victor-sha <kassini_90@mail.ru>
2019-05-19 00:46:11 +0300

    add layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const About = () => {
   return (
-    <div>
-      <Header></Header>
+    <Layout>
       <h1>About</h1>
       <p>This is about gatsby bootcamp</p>
       <p><Link to='/contact'>Contact me</Link></p>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const Contact = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Contact</h1>
       <p>Email: kassini_90@mail.ru</p>
       <p>Twitter: <a href='https://twitter.com/@shaposhnikov' target='link'>@shaposhnikov</a></p>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
