de880aa128ed2770f51b69549b2b3aef4a3090bb
victor-sha <kassini_90@mail.ru>
2019-05-19 02:03:33 +0300

    add basic scss styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Footer from '../components/footer';
 import Header from '../components/header';
+import '../styles/index.scss'
 
 const Layout = (props) => {
   return (
